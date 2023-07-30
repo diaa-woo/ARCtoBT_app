68f1be2810a0cc287c6732eb8a870e905c0007aa
diadntjr <dev.dntjr0501@gmail.com>
2023-07-30 21:19:28 +0900

    Add Session
    Add HTTP connection session part

---
+++ lib/backend/session.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class Session {
+  Map<String, String> headers = {
+    'Content-Type': 'application/json',
+    'Accept' : 'application/json',
+  };
+
+  Map<String, String> cookies = {};   // 쿠키 대신 키 값을 넣어줄 예정
+
+  Future<dynamic> post(String url, dynamic data) async {
+    print('post() url: $url');
+
+    http.Response response = await http.post(
+      Uri.encodeFull(url) as Uri,
+      body: json.encode(data),
+      headers: headers
+    );
+
+    final int statusCode = response.statusCode;
+    if (statusCode < 200 || statusCode > 400 || json == null) {
+      // TODO: input
+    }
+    return json.decode(utf8.decode(response.bodyBytes));
+  }
+}
+
